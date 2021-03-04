5761f4d12d070aeb6e11ecfbdf437983d22560d0
chananya <chananyaniasoffgmail.com>
2021-03-04 21:18:08 +0200

    commit 3

--- myPython1.py
+++ myPython1.py
@@ -11,3 +11,17 @@ name=input("what is your name?")
 print(f"welcome to you {name}")
 
 print(f"{name.title()} your name has {len(name)}")
+
+for letter in name:
+    print(letter)
+
+for index, letter in enumerate(name):
+    print(f"{index+1}:{letter}")
+    if letter=="c":
+        print("my name!!")
+    else:
+        print("😊")   
+         
+
+
+
